c13346ea993cf93dc3b08d934195b2d6894e0a56
RonnieTheCoder-Engineer <personalranveersingh2011@gmail.com>
2026-06-24 11:10:42 -0700

    Fixed Windows compatibility

--- BlockDodger_1.py
+++ BlockDodger_1.py
@@ -7,6 +7,9 @@ import time
 import random
 import threading
 
+if sys.platform == "win32":
+    os.system('')
+
 current_move = None
 
 def native_mac_keyboard_listener():
@@ -18,9 +21,18 @@ def native_mac_keyboard_listener():
         old_settings = termios.tcgetattr(fd)
         try:
             tty.setcbreak(fd)
-            key = sys.stdin.read(1).lower()
-            if key in ['a', 'd', 'y', 'n']:
-                current_move = key
+            key = sys.stdin.read(1)
+
+            if key == '\033':
+                seq = sys.stdin.read(2)
+                if seq == '[D':
+                    current_move = 'a'
+                elif seq == '[C':
+                    current_move = 'd'
+            else:
+                key = key.lower()
+                if key in ['a', 'd', 'y', 'n']:
+                    current_move = key
         finally:
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
 
@@ -32,8 +44,22 @@ def check_windows_input():
     global current_move
     if sys.platform == "win32":
         import msvcrt
-        if msvcrt.kbhit():
-            current_move = msvcrt.getch().decode('utf-8').lower()
+        while msvcrt.kbhit():
+            try:
+                key_byte = msvcrt.getch()
+
+                if key_byte in (b'\xe0', b'\x00'):
+                    arrow_code = msvcrt.getch()
+                    if arrow_code == b'K':
+                        current_move = 'a'
+                    elif arrow_code == b'M':
+                        current_move = 'd'
+                else:
+                    key = key_byte.getch().decode('utf-8', errors = 'ignore').lower()
+                    if key in ['a', 'd', 'y', 'n']:
+                        current_move = key
+            except Exception:
+                pass
 
 grid_size = 10
 board_x = 5
@@ -48,13 +74,15 @@ start_time = time.time()
 last_fall_time = time.time()
 fall_delay = 0.25
 
+os.system('clear' if os.name != 'nt' else 'cls')
+
 while True:
     elapsed_time = time.time() - start_time
     if elapsed_time > 60:
         print("You won!")
         break
     elif elapsed_time > 40:
-        fall_delay = 0.05
+        fall_delay = 0.08
     elif elapsed_time > 20:
         fall_delay = 0.15
     else:
@@ -62,8 +90,8 @@ while True:
 
     grid[board_y, board_x] = 1
     grid[fall_obj_y, fall_obj_x] = 2
-    os.system('clear' if os.name != 'nt' else 'cls')
 
+    output = "\033[H"
     for row in grid:
         row_string = ""
         for cell in row:
@@ -73,7 +101,9 @@ while True:
                 row_string += "\033[44m  \033[0m"
             elif cell == 0:
                 row_string += "・"
-        print(row_string)
+        output += row_string + "\n"
+        sys.stdout.write(output)
+        sys.stdout.flush()
 
     grid[board_y, board_x] = 0
     grid[fall_obj_y, fall_obj_x] = 0
@@ -91,7 +121,7 @@ while True:
         fall_obj_x, fall_obj_y = board_x, board_y
         grid[fall_obj_y, fall_obj_x] = 2
 
-        os.system('clear' if os.name != 'nt' else 'cls')
+        output = "\033[H"
         for row in grid:
             row_string = ""
             for cell in row:
@@ -101,7 +131,9 @@ while True:
                     row_string += "\033[44m  \033[0m"
                 elif cell == 0:
                     row_string += "・"
-            print(row_string)
+            output += row_string + "\n"
+        sys.stdout.write(output)
+        sys.stdout.flush()
 
         print("\nGame Over")
         print("\nContinue? (Y/N): ")
@@ -115,7 +147,7 @@ while True:
             if current_move == 'y':
                 break
             elif current_move == 'n':
-                print("Bye")
+                print("\nBye")
                 sys.exit()
 
             time.sleep(0.05)
@@ -129,6 +161,9 @@ while True:
         start_time = time.time()
         last_fall_time = time.time()
         current_move = None
+        continue
+
+        os.system('clear' if os.name != 'nt' else 'cls')
         continue
 
     if sys.platform == "win32":
